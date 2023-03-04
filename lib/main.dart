e9cc1847f44a640504069f23f273a8576dcc2521
onkar <onkarchavan773@gmail.com>
2023-03-04 18:16:59 +0530

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/homepage.dart';
 import 'package:flutter_application_1/pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Myapp());
@@ -12,9 +13,11 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.brown),
+      theme: ThemeData(primarySwatch: Colors.brown,primaryTextTheme: GoogleFonts.latoTextTheme()),
       darkTheme: ThemeData(brightness: Brightness.dark),
+      
       // initialRoute: "/",
       routes: {
         "/": (context) => loginpage(),
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/homepage.dart';
 
 class loginpage extends StatelessWidget {
   const loginpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "login page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.green,
-            fontWeight: FontWeight.bold,
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assest/images/loginim.png",
+            fit: BoxFit.fill,
           ),
-        ),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+                color: Colors.purple,
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter passward",
+                      labelText: "password",
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  ElevatedButton(
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(iconColor: Colors.brown),
+                      onPressed: () {
+                        routes:
+                        {
+                          (context) => "/homepage";
+                        }
+                      }),
+                ],
+              )),
+        ],
       ),
     );
   }
